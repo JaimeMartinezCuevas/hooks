e225374aaf2fd68332394b5a573366be606f442e
Mazafesi0 <jaimemarcu@gmail.com>
2024-04-09 19:57:34 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-import './App.css';
+import React from 'react';
+import useCustomHook from './hooks/useFecthCharacters.js';
 
-function App() {
+const App = () => {
   const urlPokemon = 'https://pokeapi.co/api/v2/pokemon/1';
   const urlRick = 'https://rickandmortyapi.com/api/character/1';
-  
+
+  const { data: pokemonData, loading: pokemonLoading, error: pokemonError } = useCustomHook(urlPokemon);
+  const { data: rickData, loading: rickLoading, error: rickError } = useCustomHook(urlRick);
+
   return (
-    <>
-    </>
+    <div>
+      <h2>Personaje Pokemon</h2>
+      {pokemonLoading && <p>Loading...</p>}
+      {pokemonError && <p>Error: {pokemonError.message}</p>}
+      {pokemonData && (
+        <div>
+          <p>{pokemonData.name}</p>
+          <img src={pokemonData.sprites.front_default} alt={pokemonData.name} />
+        </div>
+      )}
+
+      <h2>Personaje Rick and Morty</h2>
+      {rickLoading && <p>Loading...</p>}
+      {rickError && <p>Error: {rickError.message}</p>}
+      {rickData && (
+        <div>
+          <p>{rickData.name}</p>
+          <img src={rickData.image} alt={rickData.name} />
+        </div>
+      )}
+    </div>
   );
-}
+};
 
 export default App;
---
+++ src/hooks/useFecthCharacters.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+const useCustomHook = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const json = await response.json();
+        setData(json);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useCustomHook;
